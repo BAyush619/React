9f36ae1e5ea59c621c01122397601caa14e4d4d0
BAyush619 <bhayaniayush619@gmail.com>
2026-01-29 19:56:25 +0530

    adding features

---
+++ projects/ExpenseTracker/components/AddExpense.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "../Modules/AddExpense.module.css";
+function AddExpense({ addExpense, setAddExpense }) {
+
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+
+  function handleAdd() {
+    const newItem = {
+      id: Date.now(),
+      category: category,
+      amount: Number(amount),
+    }
+
+    setAddExpense([...addExpense, newItem]);
+
+    setCategory("");
+    setAmount("");
+  }
+
+  return (
+
+    <>
+      <center className={styles.adding}>
+        <input type="text" value={category} className={styles.addSpace} placeholder="On what you spend" onChange={(event) => setCategory(event.target.value)} />
+
+        <input type="number" value={amount} className={styles.addSpace} placeholder="Enter Amount" onChange={(event) => setAmount(event.target.value)} />
+
+        <button className={styles.addExpensebtn} onClick={handleAdd}>Add Expense</button>
+      </center>
+    </>
+  )
+}
+export default AddExpense;
--- projects/ExpenseTracker/src/App.jsx
+++ projects/ExpenseTracker/src/App.jsx
@@ -2,16 +2,45 @@ import { useState } from 'react'
 import './App.css'
 import Heading from '../components/Heading'
 import Dashboard from '../components/Dashboard'
+import AddExpense from '../components/AddExpense'
+
 function App() {
   const [budget, setBudget] = useState("");
 
+  const [addExpense, setAddExpense] = useState([
+    {
+      id: 1,
+      category: "food",
+      amount: 100,
+    },
+    {
+      id: 2,
+      category: "cloth",
+      amount: 900,
+    }
+  ]);
   return (
     <>
       <center>
         <Heading></Heading>
 
         <Dashboard showBudget={budget} setBudget={setBudget}></Dashboard>
+
+        <AddExpense addExpense={addExpense} setAddExpense={setAddExpense}></AddExpense>
       </center>
+      <div className="heading">
+        <span >category</span>
+        <span >Amount</span>
+      </div>
+      {addExpense.map((item) => {
+        return (
+          <div key={item.id} className="showItems">
+            <span> {item.category}</span>
+            <span> {item.amount}</span>
+          </div>
+        );
+      })}
+
     </>
   )
 }
