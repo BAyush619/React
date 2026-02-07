1a256cc5673f7c0696a3a971e15758c1bc4f00dc
BAyush619 <bhayaniayush619@gmail.com>
2026-02-07 09:59:55 +0530

    Update App.jsx

--- projects/SocialMedia/src/App.jsx
+++ projects/SocialMedia/src/App.jsx
@@ -5,25 +5,25 @@ import Header from './components/Header'
 import SideBar from './components/SideBar'
 import PostList from './components/PostList'
 import { useState } from 'react'
+import PostListProvider from './store/Post-list-store'
 function App() {
 
   const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
     <>
-      <div className='app-container'>
+      <PostListProvider>
+        <div className='app-container'>
 
-        <SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab}></SideBar>
+          <SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab}></SideBar>
 
-        <div className='content'>
-          <Header></Header>
-          {selectedTab === "Home" ? <PostList></PostList> : <CreatePost></CreatePost>}
-
-
-          <Footer></Footer>
+          <div className='content'>
+            <Header></Header>
+            {selectedTab === "Home" ? <PostList></PostList> : <CreatePost></CreatePost>}
+            <Footer></Footer>
+          </div>
         </div>
-      </div>
-
+      </PostListProvider>
     </>
   )
 }
