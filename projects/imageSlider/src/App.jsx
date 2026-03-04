df3253f600e7c9b9f7aba01614096159af954798
BAyush619 <bhayaniayush619@gmail.com>
2026-03-04 16:29:23 +0530

    complete img Slider

--- projects/imageSlider/src/App.jsx
+++ projects/imageSlider/src/App.jsx
@@ -18,31 +18,54 @@ function App({ url, page = 1, limit = 5 }) {
         setImages(data);
       })
   }, [url]);
-  console.log(images)
+  // console.log(images)
+
+  function handleLeft(currimgId) {
+    // setCurrImage((currimgId - 1) % limit)
+
+    setCurrImage((prev) => {
+      return prev === 0 ? images.length - 1 : prev - 1
+    })
+
+  }
+  function handleRight(currimgId) {
+    // setCurrImage((currimgId + 1) % limit)
+
+    setCurrImage((prev) => {
+      return prev === images.length - 1 ? 0 : prev + 1
+    })
+  }
+
   return (
     <>
       <div className='container'>
 
-        <FaArrowAltCircleLeft className='arrow left-arrow' />
+        <FaArrowAltCircleLeft className='arrow left-arrow' onClick={() => handleLeft(currImage)} />
 
 
-        <FaArrowAltCircleRight className='arrow right-arrow' />
+        <FaArrowAltCircleRight className='arrow right-arrow' onClick={() => handleRight(currImage)} />
 
 
-        {images.map((image) => {
+        {/* {images.map((image) => {
           return <div className='img-container' key={image.id}>
             <img src={image.download_url
             } />
           </div>
-        })}
+        })} */}
+
+        {
+          images.length > 0 && (
+            <div className='img-container' key={images.id}>
+              <img src={images[currImage].download_url} />
+            </div>
+          )
+        }
 
         <div className='sliderbar'>
-          {images.map((_, index) => {
-            return <button className='slider'></button>
+          {images.map((image, index) => {
+            return <button key={image.id} className={`slider ${index === currImage ? `active` : `inactive`}`} onClick={() => setCurrImage(index)}></button>
           })}
         </div>
-
-
       </div>
     </>
   )
--- projects/imageSlider/src/main.jsx
+++ projects/imageSlider/src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App url={"https://picsum.photos/v2/list"} limit={"5"} page={"1"} />
+    <App url={"https://picsum.photos/v2/list"} limit={"10"} page={"1"} />
   </StrictMode>,
 )
